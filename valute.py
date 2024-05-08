6fb0fcae40e023067962c7930ab5b9bab7d0236d
lemin6 <jeongseaa@gmail.com>
2024-05-09 00:41:31 +0600

    new proect

--- valute.py
+++ valute.py
@@ -7,7 +7,6 @@ amount = float(input("Введите сумму для конвертации: "
 start_currency = input("Введите стартовую валюту (USD, EUR, RUB): ").upper()
 target_currency = input("Введите целевую валюту (USD, EUR, RUB): ").upper()
 
-# Проверяем наличие введенных валют в словаре курсов
 if start_currency not in exchange_rates or target_currency not in exchange_rates:
     print("Ошибка: Введена некорректная валюта.")
 else:
